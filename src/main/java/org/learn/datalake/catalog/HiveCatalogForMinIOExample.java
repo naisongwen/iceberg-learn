377fb4f40c500318bf07e8fbfaa500b83fa03c9f
闻乃松 <wennaisong@deepexi.com>
2022-06-16 15:34:35 +0800

    iceberg 1.13.1

--- src/main/java/org/learn/datalake/catalog/HiveCatalogForMinIOExample.java
+++ src/main/java/org/learn/datalake/catalog/HiveCatalogForMinIOExample.java
@@ -22,7 +22,6 @@ import org.apache.iceberg.Table;
 import org.apache.iceberg.catalog.Catalog;
 import org.apache.iceberg.catalog.TableIdentifier;
 import org.apache.iceberg.flink.CatalogLoader;
-import org.apache.iceberg.hadoop.HadoopCatalog;
 import org.apache.iceberg.hive.HiveCatalog;
 import org.apache.iceberg.types.Types;
 
@@ -61,25 +60,25 @@ public class HiveCatalogForMinIOExample {
     }
     Configuration conf = new Configuration();
     conf.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem");
-    conf.set("fs.s3a.access.key", "admin123");
-    conf.set("fs.s3a.secret.key", "admin123");
-    conf.set("fs.s3a.endpoint", "http://10.201.0.212:34345");
+    conf.set("fs.s3a.access.key", "admin1234");
+    conf.set("fs.s3a.secret.key", "admin1234");
+    conf.set("fs.s3a.endpoint", "http://10.201.0.212:32000");
     conf.set("fs.s3a.path.style.access", "true");
     conf.set("fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider");
-    String warehouse = "s3a://tmp/iceberg/warehouse";
+    String warehouse = "s3a://faas-ethan/iceberg/warehouse";
 
     S3AFileSystem s3AFileSystem= (S3AFileSystem) S3AFileSystem.get(new URI(warehouse), conf);
     s3AFileSystem.delete(new Path(warehouse),true);
 
     setupMetastoreDB("jdbc:derby:" + getMetastorePath() + ";create=true");
     HiveConf hiveConf = new HiveConf(conf, HiveCatalogForMinIOExample.class);
-    String thriftUri="thrift://master:9083";
+    String thriftUri="thrift://localhost:51066";
     TableIdentifier tbl2 = TableIdentifier.of("default", "tbl_4");
     Map<String, String> properties = new HashMap<>();
     properties.put("type", "iceberg");
     properties.put("property-version", "1");
     properties.put("catalog-type", "hive");
-    // properties.put("uri", thriftUri);
+    properties.put("uri", thriftUri);
     properties.put("warehouse",warehouse);
     String HIVE_CATALOG = "iceberg_hive_catalog";
     // hiveConf.set(HiveConf.ConfVars.METASTOREURIS.varname, thriftUri);
